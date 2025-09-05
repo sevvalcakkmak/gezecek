27ccec8120107f31422c5433013378986138b902
Şevval Çakmak <sevvalcakmak59@gmail.com>
2025-09-05 13:57:59 +0300

    Initialize Spring Boot, set up project structure and dependencies

---
+++ backend/src/main/java/com/star/gezecek/GezecekApplication.java
@@ -0,0 +1,13 @@
+package com.star.gezecek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GezecekApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(GezecekApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/com/star/gezecek/GezecekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.star.gezecek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GezecekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
